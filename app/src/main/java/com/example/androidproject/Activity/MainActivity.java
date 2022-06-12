0a4561bdc2bb3a64216557527b446672c590a8a2
AbdallahFialah <1162193@student.birzeit.edu>
2022-06-12 13:50:04 +0300

    Add Chef Activity.........

--- app/src/main/java/com/example/androidproject/Activity/MainActivity.java
+++ app/src/main/java/com/example/androidproject/Activity/MainActivity.java
@@ -3,19 +3,13 @@ package com.example.androidproject.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
-
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.androidproject.Adaptor.CategoryAdaptor;
@@ -23,11 +17,8 @@ import com.example.androidproject.DatabaseUtility.UserSession;
 import com.example.androidproject.Domain.CategoryDomain;
 import com.example.androidproject.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 
 
